81d119ef713edb196892998c8d573eec5f589732
bigface008 <bigface00@sjtu.edu.cn>
2017-11-18 23:38:17 +0800

    Start to work on test for album.

---
+++ src/test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Album from './album.js'
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Album />, div);
+});
+
+it('', () => {
+    
+});
